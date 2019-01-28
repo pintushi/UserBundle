4994d091c8d0da7273d2dce9879409a2061de1b9
Vidy Videni <vidy.videni@gmail.com>
2019-01-28 17:46:49 +0800

    encode organization id into JWT token

--- Form/Type/UserType.php
+++ Form/Type/UserType.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace Pintushi\Bundle\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Pintushi\Bundle\UserBundle\Entity\UserInterface;
 
 abstract class UserType extends AbstractType
 {
@@ -21,10 +19,10 @@ abstract class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('plainPassword', PasswordType::class)
-            ->add('enabled', CheckboxType::class)
+            ->add('username', FormTypes\TextType::class)
+            ->add('email', FormTypes\EmailType::class)
+            ->add('plainPassword', FormTypes\PasswordType::class)
+            ->add('enabled', FormTypes\CheckboxType::class)
             ->add('gender', FormTypes\ChoiceType::class, [
                 'choices'=>[
                     '男' => UserInterface::MALE_GENDER,
